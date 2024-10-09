66db849b7aa03d4276caadf154072332d05e752b
jw-42 <v.laukman@mail.ru>
2024-10-09 14:20:59 +1000

    [feat]: add modals

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,34 @@
-import { View, SplitLayout, SplitCol, Epic } from '@vkontakte/vkui';
-import { useActiveVkuiLocation, useGetPanelForView, usePopout } from '@vkontakte/vk-mini-apps-router';
+import { View, SplitLayout, SplitCol, Epic, ModalRoot } from '@vkontakte/vkui';
+import { useActiveVkuiLocation, useGetPanelForView, usePopout, useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 
-import { VIEW, DEFAULT_VIEW, GROUPS_VIEW, HELP_VIEW } from '@app/router/model';
+import { VIEW, DEFAULT_VIEW, GROUPS_VIEW, HELP_VIEW, MODALS } from '@app/router/model';
 
 import { Homepage } from '@pages/homepage';
 import { Detail } from '@pages/detail';
 import { Communities } from '@pages/communities';
 import { Help } from '@pages/help';
 import { Aside } from '@pages/aside';
+import { ModalsPage } from '@pages/modals';
 
 export const App = () => {
 
   const popout = usePopout();
+  const router = useRouteNavigator();
 
-  const {view: activeView = VIEW.DEFAULT } = useActiveVkuiLocation();
+  const {
+    view: activeView = VIEW.DEFAULT,
+    modal: activeModal
+  } = useActiveVkuiLocation();
   const activePanel = useGetPanelForView(activeView || "default");
 
+  const Modals = (
+    <ModalRoot activeModal={activeModal} onClose={() => router.hideModal()}>
+      <ModalsPage.DetailModal id={MODALS.DETAIL} />
+    </ModalRoot>
+  )
+
   return (
-    <SplitLayout popout={popout}>
+    <SplitLayout popout={popout} modal={Modals}>
       <SplitCol autoSpaced style={{ marginLeft: 0 }}>
         <Epic activeStory={activeView || VIEW.DEFAULT}>
           <View id={VIEW.DEFAULT} activePanel={activePanel || DEFAULT_VIEW.HOMEPAGE}>
---
+++ src/widgets/detailModals/ui/index.ts
@@ -0,0 +1,7 @@
+export { DetailName } from './name';
+export { DetailDescription } from './description';
+export { DetailPictures } from './pictures';
+export { DetailPrice } from './price';
+
+export { DetailLotsOfProperties } from './lotsOfProperties';
+export { DetailMaximumValuesForProperty } from './maximumValuesForProperty';
