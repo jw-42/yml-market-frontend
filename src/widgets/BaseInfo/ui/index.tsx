1e6b29d326c89f8937d8cc33d2598c61d52cdcb3
jw-42 <v.laukman@mail.ru>
2024-12-14 07:16:35 +1000

    [fix]: remove download link

--- src/widgets/BaseInfo/ui/index.tsx
+++ src/widgets/BaseInfo/ui/index.tsx
@@ -11,16 +11,6 @@ export const BaseInfo = () => {
 
       <Text style={{ color: baseTheme.colorTextSecondary.normal.value }}>
         Приложение позволит проверить YML-файлы, необходимые для импорта товаров в магазин ВКонтакте.
-        Пример файла можно скачать <Link
-          download
-          target='_blank'
-          href='https://psv4.userapi.com/s/v1/d/uRM70YWHaR8scavBZDhscUBGcn96Eo_AEvkLweTm_HAZWe2UpN9Vmyc6oie17lEMIMAcGw1f2qocEzRaPeHeGryJMPtf_-pcoU7vEyPX9BehZjip56LG6Q/Primer_fayla_xml.txt'
-        >по ссылке</Link>.
-      </Text>
-
-      <Spacing size={16} />
-      
-      <Text style={{ color: baseTheme.colorTextSecondary.normal.value }}>
         Требования к файлу описаны в <Link target='_blank' href='https://vk.com/faq21697'>этой статье</Link>.
       </Text>
     </Div>
